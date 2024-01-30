e4e6ca00cdfcebc331a96d48c1ca1ebbf6476aa9
HimbeerserverDE <himbeerserverde@gmail.com>
2024-01-30 18:56:39 +0100

    Revert "debug event loop"
    This reverts commit c63e11ef98a689ed749e2c5f356a5d480d7681a5.

--- src/main.rs
+++ src/main.rs
@@ -156,7 +156,6 @@ async fn main() -> Result<()> {
 
     let mut buf = [0; 1500];
     loop {
-        println!("[dbg] loop select");
         tokio::select! {
             biased;
 
@@ -230,7 +229,6 @@ async fn main() -> Result<()> {
                 logged_handle(&mut dhcp6, &mut dhcp6c, buf);
             }
         }
-        println!("[dbg] loop end");
     }
 }
 
